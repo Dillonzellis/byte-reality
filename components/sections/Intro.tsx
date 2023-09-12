94cdebed5c3b9007172f518de47d4b015c94711b
dillonzellis <dillonzellis@gmail.com>
2023-09-12 16:52:51 -0400

    add ui components to Intro section

--- components/sections/Intro.tsx
+++ components/sections/Intro.tsx
@@ -1,38 +1,43 @@
 import Image from "next/image";
 import introImg from "@/public/bite-reality.jpg";
+import { Container } from "../ui/Container";
+import { Section } from "../ui/Section";
 
 export const Intro = () => {
   return (
-    <section>
+    <Section sectionName="intro">
       <div className="tw-bg-brandingBlue-400">
-        <div className="tw-grid tw-grid-cols-2">
-          <Image src={introImg} alt="intro image" />
-          <div className="tw-text-white tw-space-y-2">
-            <div className="tw-text-3xl tw-font-bold tw-pb-4">
-              Fun, Safe, and Interactive
-            </div>
-            <div>
-              Bite of Reality is a hands-on app-based simulation that appeals to
-              teens while giving them a taste of real-world financial realities.
-              Teens are given a fictional occupation, salary, credit score,
-              spouse and a child, student loan debt, credit card debt, and
-              medical insurance payments. The teens then walk around to various
-              table-top stations to “purchase” housing, transportation, food,
-              clothing, and other needs. Fortunately, the game also includes a
-              “credit union” to help with their financial needs.
-            </div>
-            <div>
-              This interactive activity will teach teens how to make financial
-              decisions and give them a better understanding of the challenges
-              of living on a budget. Teens will have to deal with a pushy “car
-              salesperson” and a commission-based “realtor,” and weigh their
-              wants versus their needs. There’s even a “Fickle Finger of Fate”
-              that will give the students unexpected “expenses” or
-              “windfalls”—just like in real life.
+        <Container>
+          <div className="tw-grid tw-grid-cols-2">
+            <Image src={introImg} alt="intro image" />
+            <div className="tw-text-white tw-space-y-2">
+              <div className="tw-text-3xl tw-font-bold tw-pb-4">
+                Fun, Safe, and Interactive
+              </div>
+              <div>
+                Bite of Reality is a hands-on app-based simulation that appeals
+                to teens while giving them a taste of real-world financial
+                realities. Teens are given a fictional occupation, salary,
+                credit score, spouse and a child, student loan debt, credit card
+                debt, and medical insurance payments. The teens then walk around
+                to various table-top stations to “purchase” housing,
+                transportation, food, clothing, and other needs. Fortunately,
+                the game also includes a “credit union” to help with their
+                financial needs.
+              </div>
+              <div>
+                This interactive activity will teach teens how to make financial
+                decisions and give them a better understanding of the challenges
+                of living on a budget. Teens will have to deal with a pushy “car
+                salesperson” and a commission-based “realtor,” and weigh their
+                wants versus their needs. There’s even a “Fickle Finger of Fate”
+                that will give the students unexpected “expenses” or
+                “windfalls”—just like in real life.
+              </div>
             </div>
           </div>
-        </div>
+        </Container>
       </div>
-    </section>
+    </Section>
   );
 };
