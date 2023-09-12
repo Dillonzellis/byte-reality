4df2e6917c86f7250926d90891fac07d8710c9ff
dillonzellis <dillonzellis@gmail.com>
2023-09-12 14:29:51 -0400

    add rough intro section

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import { Hero } from "@/components/sections/Hero";
+import { Intro } from "@/components/sections/Intro";
 
 export default function Home() {
   return (
     <main>
       <Hero />
+      <Intro />
     </main>
   );
 }
