ecf7ac034f67338a9c292950f4f500b7aabcf0ac
dillonzellis <dillonzellis@gmail.com>
2023-09-18 14:24:00 -0400

    add benefits section data

---
+++ lib/data/benefits.ts
@@ -0,0 +1,20 @@
+export const benefits = [
+  "Teaches basic money management in a safe, fun setting",
+  "Simulates everyday financial challenges and situations",
+  "Introduces the next generation to the benefits of credit unions",
+  "Helps you build relationships with schools, educators, and parents in your communities",
+  "Free to the students",
+];
+
+export const participate = [
+  "Access to the Bite of Reality2 app",
+  "Training materials for volunteers",
+];
+
+export const need = [
+  "10 volunteer adults to play the role of various merchants (per 75 students)",
+  "1 hour of on-site preparation on the day of the event",
+  "1.5 hours for simulation and debrief",
+  "50+ teens (target is grades 8–12)",
+  "A facilitator",
+];
