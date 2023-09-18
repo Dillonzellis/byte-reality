7d400eb0882b72977a7332e034d9af02a938c3a4
dillonzellis <dillonzellis@gmail.com>
2023-09-18 16:24:14 -0400

    make intro video full width

--- components/sections/VideoSection.tsx
+++ components/sections/VideoSection.tsx
@@ -4,36 +4,25 @@ import { Container } from "@/components/ui/Container";
 export const VideoSection = () => {
   return (
     <Section sectionName="videos">
-      <Container>
-        <div className="tw-grid tw-grid-cols-2 tw-gap-4">
-          <div
-            className="tw-relative tw-w-full"
-            style={{ paddingBottom: "56.25%" }}
-          >
-            <iframe
-              className="tw-absolute tw-left-0 tw-top-0 tw-h-full tw-w-full"
-              src="https://www.youtube.com/embed/gq8gqsymlH0?si=Xpccr5ii5gzVJex4"
-              title="YouTube video player"
-              frameBorder="0"
-              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-              allowFullScreen
-            ></iframe>
+      <div className="tw-bg-brandingPurple-400 tw-py-12">
+        <Container>
+          <div className="">
+            <div
+              className="tw-relative tw-w-full"
+              style={{ paddingBottom: "56.25%" }}
+            >
+              <iframe
+                className="tw-absolute tw-left-0 tw-top-0 tw-h-full tw-w-full tw-rounded-lg tw-shadow tw-shadow-zinc-800/30"
+                src="https://www.youtube.com/embed/gq8gqsymlH0?si=Xpccr5ii5gzVJex4"
+                title="YouTube video player"
+                frameBorder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                allowFullScreen
+              ></iframe>
+            </div>
           </div>
-          <div
-            className="tw-relative tw-w-full"
-            style={{ paddingBottom: "56.25%" }}
-          >
-            <iframe
-              className="tw-absolute tw-left-0 tw-top-0 tw-h-full tw-w-full"
-              src="https://www.youtube.com/embed/ppHQ9k4xAkU?si=hBWWGJumjQebFYq7"
-              title="YouTube video player"
-              frameBorder="0"
-              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-              allowFullScreen
-            ></iframe>
-          </div>
-        </div>
-      </Container>
+        </Container>
+      </div>
     </Section>
   );
 };
