d2f91264ec67ede2530ade418af0858b6928f22e
dillonzellis <dillonzellis@gmail.com>
2023-09-18 22:48:17 -0400

    add card hover translate y

--- components/sections/InfoGraphic.tsx
+++ components/sections/InfoGraphic.tsx
@@ -9,15 +9,14 @@ type CardProps = cardsContentType;
 
 const Card = ({ icon, total, content }: CardProps) => {
   return (
-    <div className="tw-group tw-flex tw-flex-col tw-items-center tw-justify-center tw-gap-6 tw-rounded-lg tw-bg-brandingBlue-400 tw-px-4 tw-py-12 tw-shadow-lg tw-shadow-zinc-800/30 tw-transition hover:tw-bg-brandingGreen-400">
+    <div className="tw-group tw-flex tw-flex-col tw-items-center tw-justify-center tw-gap-6 tw-rounded-lg tw-bg-brandingBlue-400 tw-px-4 tw-py-12 tw-shadow-lg tw-shadow-zinc-800/30 tw-transition hover:tw-translate-y-[-10px] hover:tw-bg-brandingGreen-400">
       <div className="tw-flex tw-h-32 tw-w-32 tw-items-center tw-justify-center tw-rounded-full tw-border-4 tw-border-brandingGreen-400 tw-bg-white group-hover:tw-border-brandingPurple-400">
         {icon}
       </div>
       <div className="tw-text-center tw-text-white group-hover:tw-text-zinc-800">
-        <div className="tw-pb-1 tw-font-serif tw-text-6xl tw-font-semibold">
+        <div className="tw-pb-1 tw-text-6xl tw-font-semibold tw-underline tw-decoration-brandingGreen-400 group-hover:tw-decoration-brandingPurple-400">
           {total}
         </div>
-        <hr className="tw-pb-6 group-hover:tw-border-zinc-800" />
         <div className="tw-text-xl tw-capitalize">{content}</div>
       </div>
     </div>
