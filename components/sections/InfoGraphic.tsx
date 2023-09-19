691bf5120fd303b15c78b9bc7c3e5ef623713b71
dillonzellis <dillonzellis@gmail.com>
2023-09-19 12:05:32 -0400

    update InfoGraphic with images icons

--- components/sections/InfoGraphic.tsx
+++ components/sections/InfoGraphic.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import { Container } from "@/components/ui/Container";
 import { Section } from "@/components/ui/Section";
 
@@ -11,7 +12,11 @@ const Card = ({ icon, total, content }: CardProps) => {
   return (
     <div className="tw-group tw-flex tw-flex-col tw-items-center tw-justify-center tw-gap-6 tw-rounded-lg tw-bg-brandingBlue-400 tw-px-4 tw-py-12 tw-shadow-lg tw-shadow-zinc-800/30 tw-transition hover:tw-translate-y-[-10px] hover:tw-bg-brandingGreen-400">
       <div className="tw-flex tw-h-32 tw-w-32 tw-items-center tw-justify-center tw-rounded-full tw-border-4 tw-border-brandingGreen-400 tw-bg-white group-hover:tw-border-brandingPurple-400">
-        {icon}
+        <Image
+          src={icon}
+          alt=""
+          className="tw-h-20 tw-w-20 tw-rounded-full tw-object-contain"
+        />
       </div>
       <div className="tw-text-center tw-text-white group-hover:tw-text-zinc-800">
         <div className="tw-pb-1 tw-text-6xl tw-font-semibold tw-underline tw-decoration-brandingGreen-400 group-hover:tw-decoration-brandingPurple-400">
--- lib/data/cards.ts
+++ lib/data/cards.ts
@@ -1,18 +1,20 @@
-import icon from "@/public/bite-reality.jpg";
+import single from "@/public/single-bg.png";
+import three from "@/public/three-bg.png";
+import multiple from "@/public/multiple-bg.png";
 
 export const cardsContent = [
   {
-    icon: "Events",
+    icon: single,
     total: "23",
     content: "Events held in July",
   },
   {
-    icon: "Student",
+    icon: three,
     total: "694",
     content: "students reached  in July",
   },
   {
-    icon: "Student",
+    icon: multiple,
     total: "36,052",
     content: "students reached in 2023",
   },
