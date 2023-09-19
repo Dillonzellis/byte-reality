f52811d52dac143e51ad3354dd697b996aba0c9e
dillonzellis <dillonzellis@gmail.com>
2023-09-18 21:57:24 -0400

    benefits section

--- components/sections/Benefits.tsx
+++ components/sections/Benefits.tsx
@@ -7,19 +7,33 @@ export const Benefits = () => {
   return (
     <Section sectionName="benefits">
       <Container>
-        <div className="">
-          <div className="tw-relative tw-w-[50ch] tw-rounded-md tw-bg-brandingPurple-400 tw-p-4 tw-text-2xl tw-font-semibold tw-text-white tw-shadow-lg tw-shadow-zinc-800/30 before:tw-absolute before:tw-z-[-10] before:tw-h-full before:tw-w-full before:tw-border-4 before:tw-border-brandingGreen-400 before:tw-content-['']">
-            What are the benefits of Bite of Reality?
-          </div>
-          <ul className="tw-list-inside tw-list-disc">
-            {benefits.map((li) => (
-              <li>{li}</li>
-            ))}
-          </ul>
+        <div className="tw-relative tw-mb-12 tw-w-[25ch] tw-rounded-md tw-bg-brandingBlue-400 tw-px-4 tw-py-6 tw-pr-16 tw-text-4xl tw-font-medium tw-text-white tw-shadow-lg tw-shadow-zinc-800/30 before:tw-absolute before:tw-top-[10%] before:tw-z-[-10] before:tw-h-full before:tw-w-full before:tw-border-4 before:tw-border-brandingGreen-400 before:tw-content-['']">
+          What are the{" "}
+          <span className="tw-font-bold tw-underline tw-decoration-brandingGreen-400">
+            Benefits
+          </span>{" "}
+          of Bite of Reality?
         </div>
-        <div className="tw-space-x-4">
-          <Button>event resources</Button>
-          <Button>request event code</Button>
+        <div className="tw-grid tw-grid-cols-2 tw-gap-8">
+          <div className="">
+            <ul className="tw-list-inside tw-list-disc">
+              {benefits.map((li) => (
+                <li className="tw-text-lg">{li}</li>
+              ))}
+            </ul>
+          </div>
+          <div
+            className="tw-relative tw-w-full"
+            style={{ paddingBottom: "56.25%" }}
+          >
+            <iframe
+              className="tw-absolute tw-left-0 tw-top-0 tw-h-full tw-w-full tw-rounded-lg tw-shadow tw-shadow-zinc-800/30"
+              src="https://www.youtube.com/embed/ppHQ9k4xAkU?si=hBWWGJumjQebFYq7"
+              title="YouTube video player"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              allowFullScreen
+            ></iframe>
+          </div>
         </div>
       </Container>
     </Section>
